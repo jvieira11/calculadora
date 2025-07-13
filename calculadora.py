abe1766e6bfac04b79d4ce181b973ab2f0c12bc4
Joao Victor <jvvieira2003@gmail.com>
2025-07-12 22:30:07 -0300

    calculador simples

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+# CALCULADOR SIMPLES
+
+numero1 = 0
+numero2 = 0
+resultado = 0
+op = ''
+
+while True:
+    print('CALCULADORA SIMPLES')
+    print('1 - SOMA')
+    print('2 - SUBTRAÇÃO')
+    print('3 - MULTIPLICAÇÃO')
+    print('4 - DIVISÃO')
+    print('5 - SAIR')
+
+    op = input('Escolha uma opção: ')
+
+    if op == '5':
+        print('Saindo da calculadora...')
+        break
+
+    numero1 = float(input('Digite o primeiro número: '))
+    numero2 = float(input('Digite o segundo número: '))
+
+    if op == '1':
+        resultado = numero1 + numero2
+        print(f'Resultado: {numero1} + {numero2} = {resultado}')
+    elif op == '2':
+        resultado = numero1 - numero2
+        print(f'Resultado: {numero1} - {numero2} = {resultado}')
+    elif op == '3':
+        resultado = numero1 * numero2
+        print(f'Resultado: {numero1} * {numero2} = {resultado}')
+    elif op == '4':
+        if numero2 != 0:
+            resultado = numero1 / numero2
+            print(f'Resultado: {numero1} / {numero2} = {resultado}')
+        else:
+            print('Erro: Divisão por zero não é permitida.')
+    else:
+        print('Opção inválida. Tente novamente.')
